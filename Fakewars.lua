ec4f6e1b056e460d7dcf38ce6a19ce26806a057e
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-11-28 16:08:06 +0700

    update Fakewars.lua

--- Fakewars.lua
+++ Fakewars.lua
@@ -7,16 +7,13 @@ local function chat(msg)
 	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
 
 end
+local GuiLibrary = shared.GuiLibrary
 local lplr = game.Players.LocalPlayer
 local cam = game:GetService("Workspace").CurrentCamera
 local uis = game:GetService("UserInputService")
 
-local uiCount = 0
-local scriptName = "Moon"
 
-local modules = {}
-
-local states = {}
+local scriptName = "vape"
 
 local installed = isfile(scriptName)
 if not installed then
@@ -24,189 +21,8 @@ if not installed then
 end
 local userInput = game:GetService("UserInputService")
 
-
-function newTab(name)
-	uiCount += 1
-	local ScreenGui = Instance.new("ScreenGui")
-	local Frame = Instance.new("Frame")
-	local UICorner = Instance.new("UICorner")
-	local Frame_2 = Instance.new("ScrollingFrame")
-	Frame_2.Name = "Frame"
-	Frame_2.ScrollBarThickness = 0
-	Frame_2.BorderSizePixel = 0
-	local UIGridLayout = Instance.new("UIListLayout")
-	UIGridLayout.Name = "UIGridLayout"
-	local TextLabel = Instance.new("TextLabel")
-	ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	ScreenGui.ResetOnSpawn = false
-	ScreenGui.Name = name
-	Frame.Parent = ScreenGui
-	Frame.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
-	Frame.Position = UDim2.new(0.439486176*uiCount/2.7, 0, 0.301745594, 0)
-	Frame.Size = UDim2.new(0, 157, 0, 400)
-
-	UICorner.Parent = Frame
-
-	Frame_2.Parent = Frame
-	Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Frame_2.BackgroundTransparency = 1.000
-	Frame_2.Position = UDim2.new(0.0438880287*uiCount/2.7, 0, 0.154345199, 0)
-	Frame_2.Size = UDim2.new(0, 163, 0, 303)
-
-	UIGridLayout.Parent = Frame_2
-	UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	UIGridLayout.Padding = UDim.new(0.0004,0,0,0)
-
-
-	TextLabel.Parent = Frame
-	TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabel.BackgroundTransparency = 1.000
-	TextLabel.Position = UDim2.new(0.0438881814*uiCount/2.7, 0, 0.0199366361, 0)
-	TextLabel.Size = UDim2.new(0, 163, 0, 50)
-	TextLabel.Font = Enum.Font.SourceSans
-	TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabel.TextSize = 29.000
-	TextLabel.Text = name
-	TextLabel.TextXAlignment = Enum.TextXAlignment.Left
-	userInput.InputBegan:Connect(function(input,gameProcessed)
-		if input.KeyCode == Enum.KeyCode.RightShift then
-			local check = Frame_2
-			local check2 = TextLabel
-			local check3 = Frame
-			if check.Visible == true then
-				check.Visible = false
-				check2.Visible = false
-				check3.Visible = false
-			elseif check.Visible == false then
-				check.Visible = true
-				check2.Visible = true
-				check3.Visible = true
-			end
-
-		end
-	end)
-end
-
-local windowapi = {}
-
-windowapi["CreateButton"] = function(argstablemain)
-	local buttonapi = {}
-	local player = game.Players.LocalPlayer
-	local mouse = player:GetMouse()
-
-	local bind = "nil"
-
-
-	buttonapi["Name"] = argstablemain["Name"]
-	buttonapi["Tab"] = argstablemain["Tab"]
-	buttonapi["Function"] = argstablemain["Function"]
-	--local enabled = isfile(buttonapi["Name"]..".txt")
-
-	--if enabled then
-	--table.insert(modules,buttonapi["Name"])
-	--modules[buttonapi["Name"]] = true
-	--bind = readfile(buttonapi["Name"]..".txt")
-	--else
-	--table.insert(modules,buttonapi["Name"])
-	--modules[buttonapi["Name"]] = false
-	--end
-
-	local TextButton = Instance.new("TextButton")
-	local UICorner_2 = Instance.new("UICorner")
-
-	TextButton.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")[buttonapi["Tab"]].Frame.Frame
-	TextButton.BackgroundColor3 = Color3.fromRGB(85, 85, 85)
-	TextButton.Size = UDim2.new(0, 150, 0, 60)
-	TextButton.Font = Enum.Font.SourceSans
-	TextButton.Text = buttonapi["Name"]
-	TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-	TextButton.TextSize = 14.000
-	mouse.KeyDown:connect(function(key)
-		if key == bind then
-			if buttonapi["Function"] ~= true then
-				buttonapi["Function"] = true
-				table.insert(states,argstablemain["Function"])
-				states[buttonapi["Name"]] = true
-				argstablemain["Function"](true)
-				TextButton.BackgroundColor3 = Color3.fromRGB(150, 24, 222)
-			else
-				table.insert(states,argstablemain["Function"])
-				states[buttonapi["Name"]] = false
-				argstablemain["Function"](false)
-				TextButton.BackgroundColor3 = Color3.fromRGB(85, 85, 85)
-				buttonapi["Function"] = false
-			end
-		end
-	end)
-	TextButton.MouseButton1Down:Connect(function()
-
-		if buttonapi["Function"] ~= true then
-			--if not enabled then
-
-			--end
-			table.insert(states,argstablemain["Function"])
-			states[buttonapi["Name"]] = true
-			buttonapi["Function"] = true
-			TextButton.BackgroundColor3 = Color3.fromRGB(150, 24, 222)
-		else
-			table.insert(states,argstablemain["Function"])
-			states[buttonapi["Name"]] = false
-			TextButton.BackgroundColor3 = Color3.fromRGB(85, 85, 85)
-			buttonapi["Function"] = false
-		end
-		if buttonapi["Function"] ~= true then
-			argstablemain["Function"](false)
-		else
-			argstablemain["Function"](true)
-		end
-	end)
-
-	TextButton.MouseButton2Down:Connect(function()
-		local ScreenGui = Instance.new("ScreenGui")
-		local TextBox = Instance.new("TextBox")
-		local UICorner = Instance.new("UICorner")
-
-		--Properties:
-
-		ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
-		TextBox.Parent = ScreenGui
-		TextBox.BackgroundColor3 = Color3.fromRGB(75, 75, 75)
-		TextBox.BackgroundTransparency = 0.050
-		TextBox.Position = UDim2.new(0.411009163, 0, 0.848780513, 0)
-		TextBox.Size = UDim2.new(0, 290, 0, 47)
-		TextBox.ZIndex = 99999
-		TextBox.Font = Enum.Font.SourceSans
-		TextBox.Text = ""
-		TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
-		TextBox.TextScaled = true
-		TextBox.TextSize = 14.000
-		TextBox.TextWrapped = true
-
-		UICorner.Parent = TextBox
-		TextBox.FocusLost:Connect(function()
-			bind = TextBox.Text
-			TextBox:Destroy()
-			--if enabled then
-			--delfile(buttonapi["Name"],scriptName)
-			--writefile(buttonapi["Name"]..".txt",bind)
-			--else
-			--writefile(buttonapi["Name"]..".txt",bind)
-			--end
-		end)
-	end)
-	UICorner_2.Parent = TextButton
-end
-
-newTab("Combat")
-newTab("Movement")
-newTab("Visuals")
-newTab("Utility")
-
-local InstaKillExploit = windowapi.CreateButton({
+local InstaKillExploit = GuiLibrary["ObjectsThatCanBeSaved"]["Combatwindow"]["Api"].CreateOptionsButton({
 	["Name"] = "InstaKillExploit",
-	["Tab"] = "Combat",
 	["Function"] = function(callback)
 		if callback then
 			_G.InstaKillExploit = true
@@ -324,21 +140,8 @@ local InstaKillExploit = windowapi.CreateButton({
 	end,
 })
 
-local Speed = windowapi.CreateButton({
-	["Name"] = "Speed",
-	["Tab"] = "Movement",
-	["Function"] = function(callback)
-		if callback then
-			game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 120
-		else
-			game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-		end
-	end,
-})
-
-local Flight = windowapi.CreateButton({
+local Flight = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
 	["Name"] = "Flight",
-	["Tab"] = "Movement",
 	["Function"] = function(callback)
 		if callback then
 			workspace.Gravity = 0
@@ -348,9 +151,8 @@ local Flight = windowapi.CreateButton({
 	end,
 })
 
-local FunnyFly = windowapi.CreateButton({
+local FunnyFly = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
 	["Name"] = "FunnyFly",
-	["Tab"] = "Movement",
 	["Function"] = function(callback)
 		if callback then
 			_G.Velo = true
@@ -371,9 +173,8 @@ local FunnyFly = windowapi.CreateButton({
 })
 
 local AntivoidEnabled = nil
-local AntiVoid = windowapi.CreateButton({
+local AntiVoid = GuiLibrary["ObjectsThatCanBeSaved"]["WorldWindow"]["Api"].CreateOptionsButton({
 	["Name"] = "AntiVoid",
-	["Tab"] = "Utility",
 	["Function"] = function(callback)
 		if callback then
 			AntivoidEnabled = true
@@ -394,9 +195,8 @@ local AntiVoid = windowapi.CreateButton({
 })
 
 AutoToxicEnabled = nil
-local AutoToxic = windowapi.CreateButton({
+local AutoToxic = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].CreateOptionsButton({
 	["Name"] = "AutoToxic",
-	["Tab"] = "Utility",
 	["Function"] = function(callback)
 		if callback then
 			AutoToxicEnabled = true
@@ -420,9 +220,8 @@ local AutoToxic = windowapi.CreateButton({
 	end,
 })
 
-local Nuker = windowapi.CreateButton({
+local Nuker = GuiLibrary["ObjectsThatCanBeSaved"]["WorldWindow"]["Api"].CreateOptionsButton({
 	["Name"] = "Nuker",
-	["Tab"] = "Utility",
 	["Function"] = function(callback)
 		if callback then
 			AutoToxicEnabled = true
@@ -450,9 +249,8 @@ local Nuker = windowapi.CreateButton({
 	end,
 })
 
-local betterNuker = windowapi.CreateButton({
+local betterNuker = GuiLibrary["ObjectsThatCanBeSaved"]["WorldWindow"]["Api"].CreateOptionsButton({
 	["Name"] = "MegaNuker",
-	["Tab"] = "Utility",
 	["Function"] = function(callback)
 		if callback then
 			AutoToxicEnabled = true
